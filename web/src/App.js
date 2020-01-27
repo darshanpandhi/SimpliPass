bd40de3d16da95f34c2361b133c79b0c03535516
Nahiyan Naim <nahiyannaim@yahoo.com>
2020-01-26 19:01:58 -0600

    Department Selector

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Department from "./components/Department";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Department />
+      <Footer />
     </div>
   );
 }
