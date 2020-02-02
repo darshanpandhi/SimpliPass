221095d8765e299ff6f788feae184cd0db6fa0ab
Nahiyan Naim <nahiyannaim@yahoo.com>
2020-02-01 20:13:08 -0600

    Styling for Course view

--- src/components/CourseView.jsx
+++ src/components/CourseView.jsx
@@ -1,20 +1,42 @@
 import React from "react";
 import "../styles/courseView.css";
 import { getCourseInfo } from "../Utils/utils";
+import Badge from "react-bootstrap/Badge";
 
 const CourseView = props => {
   const courseInfo = getCourseInfo(props.currCourse, props.coursesList);
 
-  const renderCourseInfo = () => {
+  const renderCourseHeader = () => {
     return (
-      <>
+      <div className="header">
         <h1>
           {courseInfo.id} - {courseInfo.name}
         </h1>
         <h3>Department of {courseInfo.department}</h3>
-        <br />
-        <h3> Difficulty Level: {courseInfo.difficulty}</h3>
-      </>
+      </div>
+    );
+  };
+
+  const renderDifficulty = () => {
+    let modifier;
+
+    if (courseInfo.difficulty >= 8) {
+      modifier = "danger";
+    } else if (courseInfo.difficulty >= 5 && courseInfo.difficulty < 8) {
+      modifier = "warning";
+    } else if (courseInfo.difficulty >= 0 && courseInfo.difficulty < 5) {
+      modifier = "success";
+    }
+
+    return (
+      <div className="difficultyContainer">
+        Difficulty Level
+        <span className="difficultyNumber">
+          <Badge pill variant={modifier}>
+            {courseInfo.difficulty}
+          </Badge>
+        </span>
+      </div>
     );
   };
 
@@ -22,22 +44,22 @@ const CourseView = props => {
     let sectionRatingsList = [];
 
     for (let [key, value] of Object.entries(courseInfo.section_ratings)) {
-      sectionRatingsList.push(<p key={key}> {`${key}:  ${value}`} </p>);
+      sectionRatingsList.push(<h5 key={key}> {`${key}:  ${value}`} </h5>);
     }
 
     return (
-      <>
-        <br />
-        <h3>Section Ratings</h3>
+      <div className="sectionRatingsContainer">
+        <h3 className="sectionRatingsHeader">Section Ratings</h3>
         {sectionRatingsList}
-      </>
+      </div>
     );
   };
 
   return (
     courseInfo.length !== 0 && (
       <div className="courseViewContainer">
-        {renderCourseInfo()}
+        {renderCourseHeader()}
+        {renderDifficulty()}
         {renderSectionRatings()}
       </div>
     )
