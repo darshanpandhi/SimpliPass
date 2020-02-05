f7eb36262eb82a6c7a5f3f038d21e55502cfb19b
Nahiyan Naim <nahiyannaim@yahoo.com>
2020-02-05 16:15:23 -0600

    Tests for department and courses components

---
+++ src/tests/CourseSelector.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import CourseSelector from "../components/CourseSelector";
+import { coursesList } from "./testData";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("CourseSelector", () => {
+  it("should render a selector for Courses based on provided coursesList and the current Department selected", () => {
+    const component = shallow(
+      <CourseSelector currDept={"Computer Science"} coursesList={coursesList} />
+    );
+
+    expect(component.prop("placeholder")).toEqual("Select a Course");
+    expect(component.prop("options")).toEqual([
+      { value: "COMP 2140", label: "COMP 2140" },
+      { value: "COMP 1010", label: "COMP 1010" }
+    ]);
+  });
+});
---
+++ src/tests/testData.js
@@ -0,0 +1,33 @@
+export const coursesList = [
+  {
+    id: "COMP 2140",
+    department: "Computer Science",
+    difficulty: 6,
+    name: "Data Structures and Algorithms",
+    section_ratings: {
+      A: 6,
+      B: 8,
+      C: 9
+    }
+  },
+  {
+    id: "COMP 1010",
+    department: "Computer Science",
+    difficulty: 2,
+    name: "Introductory Computer Science 1",
+    section_ratings: {
+      A: 9,
+      B: 3,
+      C: 2,
+      D: 4,
+      E: 5
+    }
+  },
+  {
+    id: "MATH 1700",
+    department: "Mathematics",
+    difficulty: 8,
+    name: "Calculus 2",
+    section_ratings: { A: 7, B: 5 }
+  }
+];
