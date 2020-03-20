a2d2f104905de0c354e4c9715ae7af48ca85aa56
Sabit Rahman <sabit619@gmail.com>
2020-03-20 02:52:53 -0500

    Fix navbar link issue

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
+import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, document.getElementById("root"));
 serviceWorker.unregister();
