2e3b2c87663f8ccd6f23d8df42d5c21e5d57946a
Arsh <khokharm@myumanitoba.ca>
2020-04-08 22:50:33 -0500

    Add acceptance test project

---
+++ SimpliPassMobile/SimpliPass.AcceptanceTests/AppInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace SimpliPass.AcceptanceTests
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android.InstalledApp("com.companyname.simplipassmobile").StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
